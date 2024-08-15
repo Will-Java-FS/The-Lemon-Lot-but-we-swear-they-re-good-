96c4d6920de906503685aad0eddf0383acd6c7cf
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-15 14:04:10 -0600

    added navbar

--- lemonlot-frontend/src/main.tsx
+++ lemonlot-frontend/src/main.tsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import Home from './pages/home.tsx';
+import Navbar from './pages/navbar.tsx';
+import RegisterUser from './pages/register-user.tsx';
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="register-user" element={<RegisterUser />} />
+      </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
