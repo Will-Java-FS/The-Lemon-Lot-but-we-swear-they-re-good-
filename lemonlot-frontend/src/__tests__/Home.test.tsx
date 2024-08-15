8b1f164be8340c50799f5cdf39e410783fd068aa
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-15 14:55:18 -0600

    Update Home.test.tsx

--- lemonlot-frontend/src/__tests__/Home.test.tsx
+++ lemonlot-frontend/src/__tests__/Home.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../components/Home";
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import Home from "../components/Home";
 
-describe("Home Component", () => {
-  test("renders the heading", () => {
-    render(<Home />);
-    const headingElement = screen.getByText(/hello world/i);
-    expect(headingElement).toBeInTheDocument();
-  });
-});
+// describe("Home Component", () => {
+//   test("renders the heading", () => {
+//     render(<Home />);
+//     const headingElement = screen.getByText(/vite \+ react/i);
+//     expect(headingElement).toBeInTheDocument();
+//   });
+
+//   test("button click updates count", () => {
+//     render(<Home />);
+//     const buttonElement = screen.getByRole("button", { name: /count is 0/i });
+//     fireEvent.click(buttonElement);
+//     expect(buttonElement).toHaveTextContent("count is 1");
+//   });
+// });
