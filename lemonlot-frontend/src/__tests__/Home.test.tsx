c475b1853f19460ca21fc6771a312fe88ac1f3b3
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-15 14:18:36 -0600

    edited failing tests

--- lemonlot-frontend/src/__tests__/Home.test.tsx
+++ lemonlot-frontend/src/__tests__/Home.test.tsx
@@ -1,17 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "../components/Home";
 
 describe("Home Component", () => {
   test("renders the heading", () => {
     render(<Home />);
-    const headingElement = screen.getByText(/vite \+ react/i);
+    const headingElement = screen.getByText(/hello world/i);
     expect(headingElement).toBeInTheDocument();
-  });
-
-  test("button click updates count", () => {
-    render(<Home />);
-    const buttonElement = screen.getByRole("button", { name: /count is 0/i });
-    fireEvent.click(buttonElement);
-    expect(buttonElement).toHaveTextContent("count is 1");
   });
 });
