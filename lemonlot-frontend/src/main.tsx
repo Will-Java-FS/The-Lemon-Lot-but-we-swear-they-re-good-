89743dac56a31ed5e1891362e0233e1a085cfc50
Trey Crossley <treycrossley@gmail.com>
2024-08-15 13:12:16 -0700

    Revert "added navbar"
    This reverts commit 96c4d6920de906503685aad0eddf0383acd6c7cf.

--- lemonlot-frontend/src/main.tsx
+++ lemonlot-frontend/src/main.tsx
@@ -1,21 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 import "./index.css";
-import Home from './pages/home.tsx';
-import Navbar from './pages/navbar.tsx';
-import RegisterUser from './pages/register-user.tsx';
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route index element={<Home />} />
-        <Route path="register-user" element={<RegisterUser />} />
-      </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
