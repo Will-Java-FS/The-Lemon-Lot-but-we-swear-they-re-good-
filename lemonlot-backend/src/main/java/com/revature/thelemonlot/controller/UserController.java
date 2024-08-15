87258710555a4bb27284b1a492f3a4a4340081f0
Trey Crossley <treycrossley@gmail.com>
2024-08-15 14:14:33 -0700

    UserController register endpoint

--- lemonlot-backend/src/main/java/com/revature/thelemonlot/controller/UserController.java
+++ lemonlot-backend/src/main/java/com/revature/thelemonlot/controller/UserController.java
@@ -2,9 +2,12 @@ package com.revature.thelemonlot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +34,23 @@ public class UserController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> registerUser(@RequestBody User user) {
+        if (userService.existsByUsername(user.getUsername()))
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("User already exists");
+
+        if (user.getRole() == null || user.getRole().isEmpty())
+            user.setRole("USER");
+
+        try {
+            User savedUser = userService.save(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while registering the user: " + e.getMessage());
+        }
+    }
+
 }
