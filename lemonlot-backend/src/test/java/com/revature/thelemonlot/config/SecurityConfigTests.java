45e3d18e26c5becb1282703e044b9112a0049673
Trey Crossley <treycrossley@gmail.com>
2024-08-16 18:21:57 -0700

    wait securityconfig tests but fr

--- lemonlot-backend/src/test/java/com/revature/thelemonlot/config/SecurityConfigTests.java
+++ lemonlot-backend/src/test/java/com/revature/thelemonlot/config/SecurityConfigTests.java
@@ -4,31 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.context.WebApplicationContext;
 
-import com.revature.thelemonlot.ThelemonlotApplication;
-
-@SpringBootTest(classes = ThelemonlotApplication.class) // Use main application class
+@SpringBootTest
 @TestPropertySource(properties = {
-        "SPRING_DATASOURCE_URL=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "SPRING_DATASOURCE_USERNAME=sa",
-        "SPRING_DATASOURCE_PASSWORD=password"
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.username=sa",
+        "spring.datasource.password="
 })
-class SecurityConfigTests {
+public class SecurityConfigTests {
 
     @Autowired
-    private WebApplicationContext context; // Inject the web application context
-
-    @Autowired
-    private SecurityConfig securityConfig; // Autowire the SecurityConfig
+    private SecurityFilterChain securityFilterChain;
 
     @Test
-    void testSecurityFilterChainBean() throws Exception {
-        HttpSecurity httpSecurity = context.getBean(HttpSecurity.class); // Get the HttpSecurity bean
-        SecurityFilterChain securityFilterChain = securityConfig.securityFilterChain(httpSecurity);
+    public void testSecurityFilterChainBean() {
         assertNotNull(securityFilterChain, "SecurityFilterChain bean should not be null");
     }
 }
