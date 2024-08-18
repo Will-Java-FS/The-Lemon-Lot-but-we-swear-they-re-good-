18f259cf9a33c166006c5b8abf0d7069183c674d
Trey Crossley <treycrossley@gmail.com>
2024-08-17 20:39:48 -0700

    condense logic

--- lemonlot-backend/src/test/java/com/revature/thelemonlot/config/SecurityConfigTests.java
+++ lemonlot-backend/src/test/java/com/revature/thelemonlot/config/SecurityConfigTests.java
@@ -7,15 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "SECRET_KEY=P0YfGdONKAXq8bHFO6IoIHZOhkPiNEeOi1dvnf+Ot9M="
-})
+@ActiveProfiles("test")
 public class SecurityConfigTests {
 
     @Autowired
